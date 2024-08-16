d8a77f4ca100224f9de0fc3425fcf9e4c6f11773
LJY <105623744+ljyljy020202@users.noreply.github.com>
2024-08-16 10:54:07 +0900

    [Bronze V] Title: ??!, Time: 180 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println(str+"??!");
+    }
+}
