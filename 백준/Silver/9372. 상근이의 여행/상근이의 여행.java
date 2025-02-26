955b128382c591e428ec60cf9227c7cb8bb95551
LJY <105623744+ljyljy020202@users.noreply.github.com>
2025-02-26 21:05:45 +0900

    [Silver IV] Title: 상근이의 여행, Time: 948 ms, Memory: 233792 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,29 +1,20 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
 
         int T = scan.nextInt();
-        while(T-->0){
+        while (T-- > 0) {
             int n = scan.nextInt();
             int m = scan.nextInt();
-            ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-            boolean[] visited = new boolean[n+1];
-            for(int i=0;i<=n;i++)
-                graph.add(new ArrayList<Integer>());
-            while(m-->0){
+            while (m-- > 0){
                 int a = scan.nextInt();
                 int b = scan.nextInt();
-                graph.get(a).add(b);
-                graph.get(b).add(a);
             }
-            sb.append(n-1+"\n");
+            sb.append(n - 1 + "\n");
         }
 
         System.out.println(sb);
