67d58c729e0fe9acabb33080b8d9ef91198abd9a
Lim Jiye <105623744+ljyljy020202@users.noreply.github.com>
2026-03-06 17:46:08 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 276 ms, Memory: 24160 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -6,28 +6,17 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> left = new Stack<>();
-        Stack<Integer> right = new Stack<>();
-
-        int n = Integer.parseInt(br.readLine());
-        for(int i=0;i<n;i++){
-            left.push(Integer.parseInt(br.readLine()));
-        }
-        while(!left.isEmpty()){
-            right.push(left.pop());
-        }
-
+        Stack<Integer> stack = new Stack<>();
         long cnt = 0;
-        left.push(right.pop());
-        while(!right.isEmpty()){
-            int tmp = right.pop();
-            while(!left.isEmpty() && tmp>=left.peek()){
-                left.pop();
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            int height = Integer.parseInt(br.readLine());
+            while(!stack.isEmpty() && stack.peek()<=height){
+                stack.pop();
             }
-            cnt += left.size();
-            left.push(tmp);
+            cnt += stack.size();
+            stack.push(height);
         }
-
         System.out.println(cnt);
     }
 }
