52d4ccda106aa384a5f9dbe0fdb624179c5f6cb4
Lim Jiye <105623744+ljyljy020202@users.noreply.github.com>
2026-03-09 21:43:43 +0900

    [Gold V] Title: AC, Time: 836 ms, Memory: 119216 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,57 +10,54 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        ArrayDeque<Integer> deque;
+        Deque<Integer> deq;
 
         int t = Integer.parseInt(br.readLine());
 
         while(t-->0){
-            String p = br.readLine();
+            String funcList = br.readLine();
             int n = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine(), "[],");
 
-            deque = new ArrayDeque<>();
-            for(int i = 0; i < n; i++) {
-                deque.add(Integer.parseInt(st.nextToken()));
-            }
+            deq = new ArrayDeque<>();
+            for(int i=0; i<n; i++)
+                deq.add(Integer.parseInt(st.nextToken()));
 
-            boolean reverse = false;
+            boolean reversed = false;
             boolean error = false;
 
-            for(int j=0;j<p.length();j++){
-                char ch = p.charAt(j);
-                if(ch=='R')
-                    reverse = !reverse;
+            for(int i=0; i<funcList.length() && !error; i++){
+                String func = String.valueOf(funcList.charAt(i));
+                if(func.equals("R"))
+                    reversed = !reversed;
                 else{
-                    if(!reverse) {
-                        if(deque.poll() == null)
-                            error = true;
-                    }
-                    else{
-                        if(deque.pollLast() == null)
-                            error = true;
-                    }
+                    if(deq.isEmpty())
+                        error = true;
+                    else if(reversed)
+                        deq.pollLast();
+                    else deq.poll();
                 }
             }
-
-            if(error){
+            if(error)
                 sb.append("error\n");
-            }else{
-                sb.append('[');
-                if(deque.size()>0){
-                    if(!reverse){
-                        sb.append(deque.poll());
-                        while(!deque.isEmpty())
-                            sb.append(',').append(deque.poll());
-                    }else{
-                        sb.append(deque.pollLast());
-                        while(!deque.isEmpty())
-                            sb.append(',').append(deque.pollLast());
-                    }
-                }
-                sb.append(']').append('\n');
-            }
+            else buildArr(deq, reversed, sb);
         }
         System.out.println(sb);
+    }
+
+    static void buildArr(Deque<Integer> deq, boolean reversed, StringBuilder sb){
+        sb.append("[");
+        if(!deq.isEmpty()){
+            if(reversed){
+                while(deq.size()>1)
+                    sb.append(deq.pollLast()+",");
+                sb.append(deq.pollLast());
+            }else{
+                while(deq.size()>1)
+                    sb.append(deq.poll()+",");
+                sb.append(deq.poll());
+            }
+        }
+        sb.append("]\n");
     }
 }
