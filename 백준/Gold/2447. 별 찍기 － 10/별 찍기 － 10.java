dac7740ac2a2598707d10e79c1f7b632cc107687
Lim Jiye <105623744+ljyljy020202@users.noreply.github.com>
2026-03-27 18:07:54 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 388 ms, Memory: 47800 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,14 +1,13 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
     static boolean[][] graph;
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
         graph = new boolean[n][n];
-
+        
         recursion(n,0,0);
 
         StringBuilder sb = new StringBuilder();
@@ -34,6 +33,6 @@ public class Main {
                     recursion(n/3, j, i);
             }
         }
-        
+
     }
 }
