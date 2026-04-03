33d5caa30340d554deb8e368df690a636e78206d
Lim Jiye <105623744+ljyljy020202@users.noreply.github.com>
2026-04-04 01:23:41 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -7,30 +7,31 @@ public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
         int[][] cost = new int[n][3];
+
         for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<3;j++){
+            for(int j=0;j<3;j++)
                 cost[i][j] = Integer.parseInt(st.nextToken());
-            }
         }
 
-        for(int i=1;i<n;i++){
-            for(int j=0;j<3;j++){
-                int a = (j+1)%3;
-                int b = (j+2)%3;
-                int sum_a = cost[i][j]+cost[i-1][a];
-                int sum_b = cost[i][j]+cost[i-1][b];
+        int[][] dp = new int[n][3];
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
 
-                cost[i][j] = Math.min(sum_a,sum_b);
-            }
+        for(int i=1;i<n;i++){
+            dp[i][0] += cost[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);
+            dp[i][1] += cost[i][1] + Math.min(dp[i-1][0],dp[i-1][2]);
+            dp[i][2] += cost[i][2] + Math.min(dp[i-1][0],dp[i-1][1]);
         }
 
         int min = 1000001;
         for(int j=0;j<3;j++){
-            if(cost[n-1][j]<min)
-                min = cost[n-1][j];
+            if(dp[n-1][j]<min)
+                min = dp[n-1][j];
         }
 
         System.out.println(min);
